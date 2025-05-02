cd6d4c4fe5b7827f0cf94b2bcb242c11bb906c92
argo-rich <rich@argohaus.com>
2025-05-02 11:02:08 -0500

    LTH-194 Simplified auth interceptor

--- ClientApp/src/app/_interceptors/auth.interceptor.ts
+++ ClientApp/src/app/_interceptors/auth.interceptor.ts
@@ -2,16 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { User } from '@app/_models/user';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  let idToken = '';
   let user: User = JSON.parse(localStorage.getItem('user')!);
 
   if (user && user.token) {
-    idToken = user.token;
-  }
-
-  if (idToken) {
     const cloned = req.clone({
-      headers: req.headers.set("Authorization", "Bearer " + idToken)
+      headers: req.headers.set("Authorization", "Bearer " + user.token)
     });
 
     return next(cloned);
